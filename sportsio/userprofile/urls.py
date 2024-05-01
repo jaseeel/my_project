47ca6fc41b58c5b191ae55f359c0d2e708660b91
Mohammed Jaseel <jaseeljachu@gmail.com>
2024-05-01 18:34:41 +0530

    Added user profile and manage Address

--- sportsio/userprofile/urls.py
+++ sportsio/userprofile/urls.py
@@ -10,5 +10,6 @@ from userprofile import views
 
 urlpatterns = [
     path('', views.user_profile,name="user_profile"),
-    path('add_address/', views.add_address,name="add_address")
+    path('add_address/', views.add_address,name="add_address"),
+    path('edit_address/<int:id>', views.edit_address, name="edit_address")
     ]
--- sportsio/userprofile/views.py
+++ sportsio/userprofile/views.py
@@ -7,24 +7,23 @@ from admin_side.views import *
 
 def user_profile(request):
     user=request.user
-    # if not user.is_authenticated:
-    #     redirect('login')
-    # if user.is_authenticated:
-    # address=address.objects.filter(username=user)
-    # 'address':address
-    context={
-         'user':user
-         
-     }
-    return render(request,"user_side/user_profile.html",context)
-#  else:
-#      return render('user_side/main.html')
+    if user.is_authenticated:
+        addresses=address.objects.filter(username=user)
+        context={
+             'user':user,
+             'address':addresses,
+         }
+        return render(request,"user_side/user_profile.html",context)
+    else:
+        return redirect('login')
 
+
+#__________________Add Address_______________
 def add_address(request):
     user = request.user
-    addresses = address.objects.filter(username=user)
 
     if request.method == "POST":
+        addresses=request.POST.get("address")
         house_name = request.POST.get("house_name")
         street = request.POST.get("street")
         city = request.POST.get("city")
@@ -34,6 +33,7 @@ def add_address(request):
         # Create a new Address object associated with the current user
         address.objects.create(
             username=user,
+            address=addresses,
             house_name=house_name,
             street=street,
             city=city,
@@ -45,42 +45,23 @@ def add_address(request):
         return redirect("user_profile")
     return render(request, "user_side/add_address.html")
 
-    
-        
-    
 
+#_______________ Edit Address____________
 
-
-
-
-#__________________ Manage Address  __________________
-
-def manage_address(request):
-    user = request.user
-    addresses = address.objects.filter(username=user)
-
-    if request.method == "POST":
-        house_name = request.POST.get("house_name")
-        street = request.POST.get("street")
-        city = request.POST.get("city")
-        state = request.POST.get("state")
-        zip_code = request.POST.get("zip")
-
-        # Create a new Address object associated with the current user
-        address.objects.create(
-            username=user,
-            house_name=house_name,
-            street=street,
-            city=city,
-            state=state,
-            zipcode=zip_code,
-        )
-
-        # Update the addresses queryset to include the new address
-        return redirect("user_profile:manage_address")
-
-    context = {
-        "Address": addresses,
+def edit_address(request, id):
+    addy=address.objects.get(id=id)
+    context={
+        'addy':addy
     }
-    return render(request, "user_auth/manage_address.html", context)
-
+    if request.method == "POST":
+        addy.address=request.POST.get("address")
+        addy.house_name = request.POST.get("house_name")
+        addy.street = request.POST.get("street")
+        addy.city = request.POST.get("city")
+        addy.state = request.POST.get("state")
+        addy.zip_code = request.POST.get("zip")
+    
+        addy.save()
+        return redirect('user_profile')
+    return render(request,"user_side/edit_address.html",context)
+        
