42d826a49219171885493450559fa07ad71c82ad
Mohammed Jaseel <jaseeljachu@gmail.com>
2024-10-29 15:47:43 +0530

    static changes

--- sportsio/sportsio/urls.py
+++ sportsio/sportsio/urls.py
@@ -40,3 +40,5 @@ urlpatterns = [
     path('auth/', include('social_django.urls', namespace='social')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
